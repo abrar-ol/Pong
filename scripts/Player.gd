2217bc2cea1b63acea1234456eae0afbab0d3283
Abrar Alolyan <abrar-ol@users.noreply.github.com>
2024-06-12 15:32:40 +0300

    limit the player to the height of the window

---
+++ scripts/Player.gd
@@ -0,0 +1,17 @@
+extends StaticBody2D
+
+var p_height:int
+var win_height:int
+
+func _ready():
+	p_height = $ColorRect.get_size().y
+	win_height = get_viewport_rect().size.y
+	
+func _process(delta):
+	if Input.is_action_pressed("ui_down"):
+		position.y += get_parent().PADEL_SPEED * delta
+		print(win_height)
+	if Input.is_action_pressed("ui_up"):
+		position.y -= get_parent().PADEL_SPEED * delta
+		
+	position.y = clamp(position.y,p_height/2,win_height-(p_height/2))
---
+++ scripts/main.gd
@@ -0,0 +1,4 @@
+extends Sprite2D
+
+var scores := [0,0]
+var PADEL_SPEED := 500
